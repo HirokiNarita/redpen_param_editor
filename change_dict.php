505dfea2f2ad08e941ad64151ca713d2005260f9
Hiroki Narita <k17099kk@aitech.ac.jp>
2019-12-16 22:49:36 +0900

    辞書の編集機能を実装

--- change_dict.php
+++ change_dict.php
@@ -1,22 +1,27 @@
-<!DOCTYPE html>
 <?php
-# ページが開いたと同時に読み込み
+$dict = '';
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $dict = $_POST['dict'];
+    echo "編集しました";
+  } else {
+    throw new \RuntimeException('編集に失敗しました．');
+  }
 $dict_path = "/Applications/MAMP/htdocs/redpen_param_editor/redpen/dict/SuggestExpressionDictionary.txt";
-$dict = file_get_contents($dict_path);
-if ($dict === false) {
-    throw new \RuntimeException('辞書ファイルが読み込めません．');
-}
+file_put_contents($dict_path, $dict);
 ?>
 
+<!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-<title>redpen_param_editor</title>
+  <meta charset="utf-8">
+  <title>redpen_param_editor</title>
 </head>
 <body>
+    <br>
     登録辞書の変更<br>
     <form action="change_dict.php", method="POST">
         <textarea name="dict" cols="100" rows="50"><?php echo $dict?></textarea>
         <input type="submit" value="変更" name="submit">
     </form>
 </body>
+</html>
